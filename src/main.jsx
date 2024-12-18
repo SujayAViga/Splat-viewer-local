bad3403542cff3c37858ab9a25ce21026e909a08
SujayAViga <sujay.amberkar@vigaet.com>
2024-12-19 01:55:17 +0530

    pwa setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { GlobalVariablesProvider } from '../GlobalVariables.jsx';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 createRoot(document.getElementById('root')).render(
     <GlobalVariablesProvider>
       <App />
     </GlobalVariablesProvider>
 )
+
+// Register service worker for PWA functionality
+serviceWorkerRegistration.register();
