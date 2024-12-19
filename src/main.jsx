e58d262a0ec9d7b64ebae563cbbb825bc6908c47
SujayAViga <sujay.amberkar@vigaet.com>
2024-12-19 10:17:24 +0530

    vite service workers

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { GlobalVariablesProvider } from '../GlobalVariables.jsx';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 createRoot(document.getElementById('root')).render(
@@ -10,6 +9,3 @@ createRoot(document.getElementById('root')).render(
       <App />
     </GlobalVariablesProvider>
 )
-
-// Register service worker for PWA functionality
-serviceWorkerRegistration.register();
